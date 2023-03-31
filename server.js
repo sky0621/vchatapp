0778394b759cd44ce3c6f779410a983772045e8b
sky0621 <sky14work@gmail.com>
2023-03-31 23:02:02 +0900

    -

--- server.js
+++ server.js
@@ -1,25 +1,18 @@
-const express = require('express')
+const express = require("express")
 const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const { v4: uuidV4 } = require('uuid')
+const server = require("http").Server(app)
+const io = require("socket.io")(server)
+const { v4: uuidV4 } = require("uuid")
 
 server.listen(process.env.PORT || 3030)
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.set("view engine", "ejs")
+app.use(express.static("public"))
 
 app.get("/", (req, res) => {
-    res.redirect(`${uuidV4()}`)
+    res.redirect(`/${uuidV4()}`)
 })
 
 app.get("/:room", (req, res) => {
     res.render("room", { roomId: req.params.room })
-})
-
-io.on("connection", (socket) => {
-    socket.on("join-room", (roomId, userId) => {
-        socket.join(roomId)
-        socket.to(roomId).broadcast.emit("user-connected", userId)
-    })
 })
