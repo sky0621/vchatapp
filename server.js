e1874188f3aa2f95cceeaa9b464908583fb3412a
sky0621 <sky14work@gmail.com>
2023-03-31 23:05:52 +0900

    -

--- server.js
+++ server.js
@@ -16,3 +16,10 @@ app.get("/", (req, res) => {
 app.get("/:room", (req, res) => {
     res.render("room", { roomId: req.params.room })
 })
+
+io.on("connection", socket => {
+    socket.on("join-room", (roomId, userId) => {
+        console.log("roomId=", roomId)
+        console.log("userId=", userId)
+    })
+})
